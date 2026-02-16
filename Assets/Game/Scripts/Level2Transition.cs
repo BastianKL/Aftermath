81d47b80ec29388c6bc3adf125cff1bab8497643
BastianKL <bastianlindholm23@gmail.com>
2026-02-16 09:33:56 +0100

    level3doneskinermest

--- Assets/Game/Scripts/Level2Transition.cs
+++ Assets/Game/Scripts/Level2Transition.cs
@@ -1,42 +1,33 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Level2Transition : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject[] lights; // Assign your light GameObjects here
-    [SerializeField] private LevelTransition levelTransition; // Reference to LevelTransition
+    [SerializeField] private BoxCollider transitionCollider; // Reference to the trigger collider
 
-    [Header("Events")]
-    public UnityEvent onAllLightsOff;
-
-    public bool canTransition = false;
-
-    private int lightsOffCount = 0;
-
-    public void LightTurnedOff()
+    private void Start()
     {
-        lightsOffCount++;
-        Debug.Log("Light turned off! Count: " + lightsOffCount);
-        if (lightsOffCount == 4 && canTransition)
+        if (transitionCollider != null)
+            transitionCollider.enabled = false;
+    }
+
+    // Call this method whenever a light is turned off
+    public void CheckLights()
+    {
+        int offCount = 0;
+        foreach (var lightObj in lights)
         {
-            onAllLightsOff.Invoke();
-            if (levelTransition != null)
-            {
-                levelTransition.StartCoroutine("TransitionToNextLevel");
-            }
+            var lightComp = lightObj.GetComponent<Light>();
+            if (lightComp != null && !lightComp.enabled)
+                offCount++;
+        }
+
+        if (offCount == lights.Length)
+        {
+            Debug.Log("All lights are off! Enabling collider.");
+            if (transitionCollider != null)
+                transitionCollider.enabled = true;
         }
     }
-
-    // Optional: Reset the counter if needed
-    public void ResetLights()
-    {
-        lightsOffCount = 0;
-    }
-
-    public void EnableTransition()
-    {
-        canTransition = true;
-    }
-
 }
